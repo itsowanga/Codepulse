b96fb08b4f4102b38b6b014ff6ada44a2c7aebd6
itsowanga <sowanga.mbane@gmail.com>
2025-11-02 17:24:36 +0200

    feat: Language, file and editor detection.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
     #include <windows.h>
     #include <winuser.h>
     #include <iostream>
-   
+    #include <string>
 
     using namespace std;
     int main(void){
@@ -14,7 +14,11 @@
 
             char processName[256];
             GetWindowText(foreground, processName, 256);
-            cout<<processName;
+            string process = (string)processName;
+            string filename = process.substr(0, process.find("-")-1);
+            string language = filename.substr(filename.find("."), filename.size()-1);
+            string editorName = process.substr(process.rfind("-")+2, process.size()-1);
+            cout<<"File: "<<filename<<'\n'<< "Extension: "<<language<<'\n'<<"Editor: "<<editorName;
         }
         return 0;
     }
