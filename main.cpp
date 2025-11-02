593800b5e3874f08dd556687d28e54e71a82189e
itsowanga <sowanga.mbane@gmail.com>
2025-11-02 18:16:12 +0200

    clean: Removed editor printing.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
     #include <windows.h>
     #include <winuser.h>
     #include <iostream>
+    #include <stdio.h>
     #include <string>
-
+    #include <sql.h>
     using namespace std;
     int main(void){
 
@@ -11,14 +12,14 @@
        
        // Enters the loop if the foreground processes is active
         if(foreground != NULL){
-
+            // Store the foreground process information
             char processName[256];
             GetWindowText(foreground, processName, 256);
             string process = (string)processName;
+            // Slice the string to find filename + language using the file extension.
             string filename = process.substr(0, process.find("-")-1);
             string language = filename.substr(filename.find("."), filename.size()-1);
-            string editorName = process.substr(process.rfind("-")+2, process.size()-1);
-            cout<<"File: "<<filename<<'\n'<< "Extension: "<<language<<'\n'<<"Editor: "<<editorName;
+            cout<<"File: "<<filename<<'\n'<< "Extension: "<<language;
         }
         return 0;
     }
